b846898aee1312ee150ad5257f0fe603f6e00f18
Peter Klipfel <peter@klipfel.me>
2013-10-27 22:00:21 -0600

    send is now NewTask

--- NewTask.java
+++ NewTask.java
@@ -2,7 +2,7 @@ import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.Channel;
 
-public class Send {
+public class NewTask {
   private final static String QUEUE_NAME = "hello";
   public static void main(String[] argv) throws java.io.IOException {
     ConnectionFactory factory = new ConnectionFactory();
@@ -11,7 +11,7 @@ public class Send {
     Channel channel = connection.createChannel();
 
     channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-    String message = "Hello World";
+    String message = getMessage(argv);
     for (int i=0; i<1000; i=i+1) {
       channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
     }
