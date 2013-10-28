e4338dc1ac6231ca551148aa1d4ee2ce81ed2add
Peter Klipfel <peter@klipfel.me>
2013-10-27 22:06:21 -0600

    unified indentation

--- NewTask.java
+++ NewTask.java
@@ -21,18 +21,18 @@ public class NewTask {
   }
 
   private static String getMessage(String[] strings){
-      if (strings.length < 1)
-          return "processing";
-      return joinStrings(strings, " ");
+    if (strings.length < 1)
+      return "processing";
+    return joinStrings(strings, " ");
   }
 
   private static String joinStrings(String[] strings, String delimiter) {
-      int length = strings.length;
-      if (length == 0) return "";
-      StringBuilder words = new StringBuilder(strings[0]);
-      for (int i = 1; i < length; i++) {
-          words.append(delimiter).append(strings[i]);
-      }
-      return words.toString();
+    int length = strings.length;
+    if (length == 0) return "";
+    StringBuilder words = new StringBuilder(strings[0]);
+    for (int i = 1; i < length; i++) {
+      words.append(delimiter).append(strings[i]);
+    }
+    return words.toString();
   }
 }
